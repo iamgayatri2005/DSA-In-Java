dc24a93f1884e09617a89ec7b2c871de21c945df
iamgayatri2005 <hiiamgayatri15@gmail.com>
2026-01-30 00:04:09 +0530

    update code

---
+++ Arrays/MinimumElement.java
@@ -0,0 +1,15 @@
+public class MinimumElement {
+    public static void main(String[] args) {
+        int[] arr = { 5, 8, 2, 9, 1 };
+
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println(min);
+    }
+}
