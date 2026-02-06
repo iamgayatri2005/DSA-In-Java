27d427b637c59994bfd6c4e319dd30d7e0124c8b
iamgayatri2005 <hiiamgayatri15@gmail.com>
2026-02-06 12:59:46 +0530

    update code

---
+++ Arrays/ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main (String args[]){
+        int[] arr = {5,8,3,6,9};
+        for(int ele: arr){
+            System.out.println(ele+ " ");
+        }
+
+    }
+}
