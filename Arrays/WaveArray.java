eb1ccd7bb616b6c264c9b09c42b4004644060412
iamgayatri2005 <hiiamgayatri15@gmail.com>
2026-02-10 12:32:35 +0530

    update code

--- Arrays/WaveArray.java
+++ Arrays/WaveArray.java
@@ -1,7 +1,7 @@
 public class WaveArray {
     public static void main(String[] args) {
 
-        // int[] arr = {1, 2, 3, 4, 5, 6};  // define the array
+        int[] arr = {1, 2, 3, 4, 5, 6};  // define the array
 
         for (int i = 0; i < arr.length - 1; i += 2) {
             int temp = arr[i];
