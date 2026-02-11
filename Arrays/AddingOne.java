88124b08d6269f480586d9a3b85dd063d1e87e3a
iamgayatri2005 <hiiamgayatri15@gmail.com>
2026-02-11 21:18:29 +0530

    update code

--- Arrays/AddingOne.java
+++ Arrays/AddingOne.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
-public class AddingOne {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+// import java.util.Scanner;
+// public class AddingOne {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter a number: ");
-        int number = scanner.nextInt();
+//         System.out.println("Enter a number: ");
+//         int number = scanner.nextInt();
 
-        int result = number + 1;
+//         int result = number + 1;
 
-        System.out.println("Result after adding 1:" 
-            + result);
+//         System.out.println("Result after adding 1:" 
+//             + result);
 
-        scanner.close();    
-    }
-}
+//         scanner.close();    
+//     }
+// }
