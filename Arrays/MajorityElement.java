33e96577a9d6fd2137a9821999e1302f50d2a27d
iamgayatri2005 <hiiamgayatri15@gmail.com>
2026-02-12 21:11:36 +0530

    update code

---
+++ Arrays/MajorityElement.java
@@ -0,0 +1,31 @@
+class MajorityElement {
+
+    static int majorityElement(int arr[]) {
+        int n = arr.length;
+
+        for (int val : arr) {
+            int freq = 0;
+
+            for (int el : arr) {
+                if (el == val) {
+                    freq++;
+                }
+            }
+
+            if (freq > n / 2) {
+                return val;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {2, 2, 1, 1, 2};
+
+        int result = majorityElement(arr);
+
+        System.out.println("Majority Element: " + result);
+    }
+}
